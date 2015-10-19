e609cda274ef5e04ee4185cb88ef7bb28f1ad314
Vince <vincent.falk@tufts.edu>
2015-10-19 13:52:37 -0400

    Working on Lab 6

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,17 @@
-// Your JavaScript goes here...
+// Lab 6 js file containing parse function for json data
+
+var jsondata = new XMLHttpRequest();
+jsondata.open("GET","data.json",true);
+
+
+function parse() {
+
+	parsedObjects = JSON.parse(jsondata);
+	elem = document.getElementByID("messages");
+
+	for( count = 0; count < parsedObjects.length; count++){
+		console.log(Objects.keys(parsedObjects[count]));
+		elem.innerHTML += "<p> " + parsedObjects[count]["content"] + parsedObects[count]["username"] + "</p>";
+	}
+
+}
