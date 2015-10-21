ba3da3589f7ea99212819714cc808da25c381338
Vince <vincent.falk@tufts.edu>
2015-10-20 22:01:30 -0400

    Added paragraph tags

--- messages/lab.js
+++ messages/lab.js
@@ -10,7 +10,7 @@ function parse() {
 		if (xhr.readyState == 4 && xhr.status == 200){
 			jsondata = JSON.parse(xhr.responseText);
 			elem = document.getElementById("messages");
-			elem.innerHTML = jsondata["content"];	
+			elem.innerHTML = <p> + jsondata["content"] + </p>;	
 		}	
 
 	}
